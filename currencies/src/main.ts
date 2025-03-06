5cec8ad4ef00a703e78ef484295b7dcb6b44adb6
aharo1110 <129776384+aharo1110@users.noreply.github.com>
2025-03-05 16:05:12 -0800

    Finish

--- currencies/src/main.ts
+++ currencies/src/main.ts
@@ -2,15 +2,39 @@ import { applets } from '@web-applets/sdk';
 
 const context = applets.register();
 
-// Define a 'set_name' action, and make it update the shared data object with the new name
-context.setActionHandler('set_name', ({ name }) => {
-  context.data = { name };
+// Convert action
+context.setActionHandler('exchange', async ({ base_symbol, target_symbol, amount }) => {
+  fetch(`https://api.frankfurter.dev/v1/latest?base=${base_symbol}&symbols=${target_symbol}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const convertBy = amount ?? 1;
+        const rate = data.rates[target_symbol];
+        const convertedAmount = (rate * convertBy).toFixed(2);
+        context.data = {
+          base_symbol,
+          target_symbol,
+          amount: convertBy,
+          result: convertedAmount
+        };
+    })
+    .catch((error) => {
+        console.error('Error fetching exchange rate:', error);
+        context.data = {
+          result: 'Error'
+        };
+    });
 });
 
 // Whenever the data is updated, update the view
 context.ondata = () => {
-  const nameElement = document.getElementById('name');
-  if (nameElement) {
-    nameElement.innerText = context.data.name;
+  if(context.data.result === 'Error') {
+    document.body.innerHTML = `
+      <div class="result">Error fetching exchange rate</div>
+    `;
+    return;
   }
+  document.body.innerHTML = `
+    <div class="conversion">${context.data.amount} ${context.data.base_symbol} in ${context.data.target_symbol}</div>
+    <div class="result">${context.data.result} ${context.data.target_symbol}</div>
+  `;
 };
